f84ddceb2d445f5152bf5aa5bebd26964d04679f
FukayaHiroki <asitanonegif@gmail.com>
2019-09-07 21:59:27 +0900

    管理ユーザーのコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/index'
+    get 'users/show'
+  end
   root to: 'tasks#index'
   resources :tasks
 end
