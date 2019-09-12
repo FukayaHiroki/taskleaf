cd6c6a1884901256b3c888b64876237b84b2ba4d
FukayaHiroki <asitanonegif@gmail.com>
2019-09-12 14:20:55 +0900

    テストコードの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
 
     if user&.authenticate(session_params[:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to root_url, notice: 'ログインしました。'
     else
       render :new
@@ -22,6 +22,6 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:sessiion).permit(:email, :password)
+    params.require(:session).permit(:email, :password)
   end
 end
